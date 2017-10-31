3d5b2fc957e429708f0ec52d2682ed099b5201d7
547274419@qq.com <547274419@qq.com>
2017-10-31 19:52:54 +0800

    abs rank

--- c/c6.cpp
+++ c/c6.cpp
@@ -1,26 +1,35 @@
 #include <stdio.h>
-int abs(int x)
-{
-	return x>0 ? x : -x;
-}
-int main(void)
-{
-	int i,j,n,tmp,a[100];
-	scanf("%d",&n);
-	do{
-	for(i=0;i<n;i++)
-	scanf("%d",&a[i]);
-	for(i=n-1; i>=1;--i){
-	for(j=0;j<i;++j)
-	if(abs(a[j])>abs(a[j+1]))
-		{tmp=a[j];a[j]=a[j+1];a[j+1]=tmp;}
-		}
-	for(i=0;i<n;++i)
-	{printf("%d",a[i]);printf(" ");}
-	printf("\n");
-	scanf("%d",&n);	
-	}while(n);
+#include <stdlib.h> 
+
+int main() 
+{	int n,a[100];
+	int i,j;
+	
+	while((scanf("%d",&n)!=EOF) && (n<=100))
+	{
+	   if(n==0)   
+	   return 0;
+	   
+	   for(i=0;i<n;i++)
+	   {scanf("%d",&a[i]);}
+		for(i=n-1;i>=0;i--)   
+  		{    for(j=0;j<i;j++) 
+		    {	if(abs(a[j]) < abs(a[j+1])) 
+			    {a[j]+=a[j+1];   
+				 a[j+1]=a[j]-a[j+1];  
+				 a[j]-=a[j+1];}
+			}  
+		}  
+		
+        for(i=0; i<n; i++)
+        {
+            if(i==0)
+                printf("%d",a[i]);
+            else
+                printf(" %d",a[i]);
+        }
+        printf("\n");
+	} 
 	
 	return 0;
- } 
- 
+}
