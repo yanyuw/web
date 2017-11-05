c0def458d7055e173812e1a8e56837f691659584
547274419@qq.com <547274419@qq.com>
2017-11-05 20:06:18 +0800

    singer

--- c/c9.cpp
+++ c/c9.cpp
@@ -2,23 +2,20 @@
 int main(void)
 {
 	int year,month,day,tmp,result,i;
-	int count[12]={31,28,31,30,31,30,31,31,30,31,30,31};
+	int count[13]={0,31,28,31,30,31,30,31,31,30,31,30,31};
 	
-	scanf("%d/%d/%d",&year,&month,&day);
-	while(year>0 && month>0 && day>0)
+	while(scanf("%d/%d/%d",&year,&month,&day)!=EOF)
 	{
     result=0;
 	if(( year%400==0 )||( year%100!=0 && year%4==0 ))
-	tmp=1;
-	else tmp=0;
-	count[2]+=tmp;
+	count[2]=29;
+	else count[2]=28;
 	
-	for(i=1;i<=12;i++)
-	if(month>i) result+=count[i];
-	result+=day;
+	for(i=0;i<month;i++)
+		result+=count[i];
+		result+=day;
 	
 	printf("%d\n",result);
-	scanf("%d/%d/%d",&year,&month,&day);
 	}
 	return 0;
     
