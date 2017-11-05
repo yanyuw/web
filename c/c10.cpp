141170b2cf2b82844cec5edf19b88f14cae2df99
547274419@qq.com <547274419@qq.com>
2017-11-05 20:05:16 +0800

    ave score

--- c/c10.cpp
+++ c/c10.cpp
@@ -1,52 +1,56 @@
-#include<stdio.h> 
-int main(void)
+#include<stdio.h>  
+double avestu[50],avesub[5];  
+double score[50][5];
+double sumstu,sumsub;  
+int i,j;  
 
-{
-	int n,m,i,j,test[50][5],count,tmp;
-	double ave1[50],ave2[5],sum;
-	
-	scanf("%d%d",&n,&m);
-	while(n>0&& n<=50 && m<=5 && m>0)
-	{
-		count=0;
-		for(i=0;i<n;i++)
-		  for(j=0;j<m;j++)
-		    scanf("%d",&test[i][j]);
-		  
-		
-		for(i=0;i<n;i++)
-		{  sum=0;
-		for(j=0;j<m;j++)
-		   sum+=test[i][j];
-		   ave1[i]=sum/m;
-		}
-		    
-		    
-		for(j=0;j<m;j++)  
-		{  sum=0;
-		for(i=0;i<n;i++)
-		      sum+=test[i][j];
-		      ave2[j]=sum/n;
-		} 
-		
-		for(i=0;i<n;i++)
-            {tmp=1; 
-            for(j=0;j<m;j++) 
-                if(test[i][j]<ave1[j])
-                { tmp=0; break; } 
-            if(tmp)
-                count++;
-			} 
-		
-		for(i=0;i<n;i++)
-		  printf("%.2lf ",ave1[i]);
-		  printf("\n");
-	    for(j=0;j<m;j++)
-		  printf("%.2lf ",ave2[j]);
-		  printf("\n");
-		printf("%d\n",count);
-			  
-	scanf("%d%d",&n,&m);
-	}
-	return 0;
+int main()  
+{  
+    int n,m;  
+    while(scanf("%d%d",&n,&m)!=EOF)  
+    {  
+        for (i=0;i<n;i++)  
+            for (j=0;j<m;j++)  
+                scanf("%lf",&score[i][j]);  
+        
+        for(i=0;i<n;i++)  
+        {  
+            sumstu=sumsub=0;  
+            for(j=0;j<m;j++)  
+            {  
+                sumstu+=score[i][j];
+            }  
+            avestu[i]=sumstu/m; 
+        }  
+        for(i=0;i<m;i++)  
+        {  
+            sumsub=0;  
+            for(j=0;j<n;j++)  
+                sumsub+=score[j][i];   
+            avesub[i]=sumsub/n;  
+        }  
+        int sum=0,temp;
+        for( i=0;i<n;i++)  
+        {  
+            temp=0;  
+            for(j=0;j<m;j++)  
+            {                 
+                if(score[i][j]<avesub[j]) 
+                {  
+                    temp=1;  
+                    break;  
+                }  
+            }  
+            if(temp==0)  
+                sum++;  
+        }  
+        for(i=0;i<n-1;i++)  
+            printf("%.2lf ",avestu[i]);  
+        printf("%.2lf\n",avestu[n-1]);  
+        for(i=0;i<m-1;i++)  
+            printf("%.2lf ",avesub[i]);  
+        printf("%.2lf\n",avesub[m-1]);  
+        printf("%d\n\n",sum);  
+    }  
+    return 0; 
 }
